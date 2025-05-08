fb151bc330f1fb2b017a8af697987e25430b2a38
Mark Meritt <mark@apptist.ca>
2025-05-08 19:16:36 -0400

    This is the website initial changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Welcome to WDDM 130")
+
+document.getElementById("para1").style.color = "yellow"
+var tags = document.getElementsByTagName("h2")
+
+for(var i = 0; i < tags.length; i++) 
+{
+    tags[i].style.color = "blue"
+    tags[i].style.textAlign = "center"
+} 
